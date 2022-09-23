bb6364cec5d67ab8af9c4806a55ca4ea07a63f13
Artur0705 <artlil420@gmail.com>
2022-09-23 17:27:16 +1000

    Added reaction model

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Requiring packages needed for this app.
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,16 +8,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 // Creating a connection
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-network",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
