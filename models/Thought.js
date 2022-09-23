f0132950e09482b656e8e39b7812460f2e3ea5c4
Artur0705 <artlil420@gmail.com>
2022-09-23 17:28:21 +1000

    Added hought model

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+const Reaction = require("./Reaction");
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
